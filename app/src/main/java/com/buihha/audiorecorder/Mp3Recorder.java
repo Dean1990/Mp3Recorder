9ae09278c287a64ebbb8ddd5927841e97f0f1236
Dean <abcruoshui@sina.com>
2018-12-04 14:46:52 +0800

    #1

--- app/src/main/java/com/buihha/audiorecorder/Mp3Recorder.java
+++ app/src/main/java/com/buihha/audiorecorder/Mp3Recorder.java
@@ -11,199 +11,214 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Mp3Recorder {
-	
-	private static final String TAG = Mp3Recorder.class.getSimpleName();
 
-	OnRecordListener mListener;
-	
-	static {
-		System.loadLibrary("mp3lame");
-	}
+    private static final String TAG = Mp3Recorder.class.getSimpleName();
 
-	private static final int DEFAULT_SAMPLING_RATE = 22050;
-	
-	private static final int FRAME_COUNT = 160;
-	
-	/* Encoded bit rate. MP3 file will be encoded with bit rate 32kbps */ 
-	private static final int BIT_RATE = 32;
+    OnRecordListener mListener;
 
-	private AudioRecord audioRecord = null;
+    static {
+        System.loadLibrary("mp3lame");
+    }
 
-	private int bufferSize;
+    private static final int DEFAULT_SAMPLING_RATE = 22050;
 
-	public File mp3File;
-	
-	private RingBuffer ringBuffer;
-	
-	private byte[] buffer;
+    private static final int FRAME_COUNT = 160;
 
-	private FileOutputStream os = null;
+    /* Encoded bit rate. MP3 file will be encoded with bit rate 32kbps */
+    private static final int BIT_RATE = 32;
 
-	private DataEncodeThread encodeThread;
+    private AudioRecord audioRecord = null;
 
-	private int samplingRate;
+    private int bufferSize;
 
-	private int channelConfig;
+    public File mp3File;
 
-	private PCMFormat audioFormat;
-	
-	private boolean isRecording = false;
+    private RingBuffer ringBuffer;
 
-	public Mp3Recorder(int samplingRate, int channelConfig,
-			PCMFormat audioFormat) {
-		this.samplingRate = samplingRate;
-		this.channelConfig = channelConfig;
-		this.audioFormat = audioFormat;
-	}
+    private byte[] buffer;
 
-	/**
-	 * Default constructor. Setup recorder with default sampling rate 1 channel,
-	 * 16 bits pcm
-	 */
-	public Mp3Recorder() {
-		this(DEFAULT_SAMPLING_RATE, AudioFormat.CHANNEL_IN_MONO,
-				PCMFormat.PCM_16BIT);
-	}
-	
-	public boolean isRecording(){
-		
-		return isRecording;
-		
-	}
+    private FileOutputStream os = null;
 
-	/**
-	 * Start recording. Create an encoding thread. Start record from this
-	 * thread.
-	 * 
-	 * @throws IOException
-	 */
-	public void startRecording(String dir, String name) throws IOException {
-		if (isRecording) return;
-		Log.d(TAG, "Start recording");
-		Log.d(TAG, "BufferSize = " + bufferSize);
-		// Initialize audioRecord if it's null.
-		if (audioRecord == null) {
-			
-			File directory = new File(dir);
-			if (!directory.exists()) {
-				directory.mkdirs();
-				Log.d(TAG, "Created directory");
-			}
-			
-			mp3File = new File(directory, name);
-			
-			initAudioRecorder();
-		}
-		audioRecord.startRecording();
-		if (mListener!=null)
-			mListener.onStart();
-		
-		new Thread() {
+    private DataEncodeThread encodeThread;
 
-			@Override
-			public void run() {
-				isRecording = true;
-				while (isRecording) {
-					int bytes = audioRecord.read(buffer, 0, bufferSize);
-					if (bytes > 0) {
-						ringBuffer.write(buffer, bytes);
-					}
-				}
-				
-				// release and finalize audioRecord
-				try {
-					audioRecord.stop();
-					audioRecord.release();
-					audioRecord = null;
-					if (mListener!=null)
-						mListener.onStop();
+    private int samplingRate;
 
-					// stop the encoding thread and try to wait
-					// until the thread finishes its job
-					Message msg = Message.obtain(encodeThread.getHandler(),
-							DataEncodeThread.PROCESS_STOP);
-					msg.sendToTarget();
-					
-					Log.d(TAG, "waiting for encoding thread");
-					encodeThread.join();
-					Log.d(TAG, "done encoding thread");
-				} catch (InterruptedException e) {
-					Log.d(TAG, "Faile to join encode thread");
-				} finally {
-					if (os != null) {
-						try {
-							os.close();
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-				
-			}			
-		}.start();
-	}
+    private int channelConfig;
 
-	/**
-	 * 
-	 * 
-	 */
-	public void stopRecording(){
-		Log.d(TAG, "stop recording");
-		isRecording = false;
-	}
+    private PCMFormat audioFormat;
 
-	/**
-	 * Initialize audio recorder
-	 */
-	private void initAudioRecorder() throws IOException {
-		int bytesPerFrame = audioFormat.getBytesPerFrame();
+    private boolean isRecording = false;
+
+    public Mp3Recorder(int samplingRate, int channelConfig,
+                       PCMFormat audioFormat) {
+        this.samplingRate = samplingRate;
+        this.channelConfig = channelConfig;
+        this.audioFormat = audioFormat;
+    }
+
+    /**
+     * Default constructor. Setup recorder with default sampling rate 1 channel,
+     * 16 bits pcm
+     */
+    public Mp3Recorder() {
+        this(DEFAULT_SAMPLING_RATE, AudioFormat.CHANNEL_IN_MONO,
+                PCMFormat.PCM_16BIT);
+    }
+
+    public boolean isRecording() {
+
+        return isRecording;
+
+    }
+
+    /**
+     * Start recording. Create an encoding thread. Start record from this
+     * thread.
+     *
+     * @throws IOException
+     */
+    public void startRecording(String dir, String name) throws IOException {
+        if (isRecording) return;
+        Log.d(TAG, "Start recording");
+        Log.d(TAG, "BufferSize = " + bufferSize);
+        // Initialize audioRecord if it's null.
+        if (audioRecord == null) {
+
+            File directory = new File(dir);
+            if (!directory.exists()) {
+                directory.mkdirs();
+                Log.d(TAG, "Created directory");
+            }
+
+            mp3File = new File(directory, name);
+
+            initAudioRecorder();
+        }
+        audioRecord.startRecording();
+        if (mListener != null)
+            mListener.onStart();
+
+        new Thread() {
+
+            @Override
+            public void run() {
+                isRecording = true;
+                while (isRecording) {
+                    //bytes是实际读取的数据长度，一般而言bytes会小于buffersize
+                    int bytes = audioRecord.read(buffer, 0, bufferSize);
+                    if (bytes > 0) {
+                        long v = 0;
+                        // 将 buffer 内容取出，进行平方和运算
+                        for (int i = 0; i < buffer.length; i++) {
+                            v += buffer[i] * buffer[i];
+                        }
+                        //平方和除以数据总长度，得到音量大小。
+                        double mean = v / (double) bytes;
+                        double volume = 10 * Math.log10(mean);
+                        Log.d(TAG, "分贝值:" + volume);
+                        if (mListener != null)
+                            mListener.onRecording(audioRecord.getSampleRate(),volume);
+                        ringBuffer.write(buffer, bytes);
+                    }
+                }
+
+                // release and finalize audioRecord
+                try {
+                    audioRecord.stop();
+                    audioRecord.release();
+                    audioRecord = null;
+                    if (mListener != null)
+                        mListener.onStop();
+
+                    // stop the encoding thread and try to wait
+                    // until the thread finishes its job
+                    Message msg = Message.obtain(encodeThread.getHandler(),
+                            DataEncodeThread.PROCESS_STOP);
+                    msg.sendToTarget();
+
+                    Log.d(TAG, "waiting for encoding thread");
+                    encodeThread.join();
+                    Log.d(TAG, "done encoding thread");
+                } catch (InterruptedException e) {
+                    Log.d(TAG, "Faile to join encode thread");
+                } finally {
+                    if (os != null) {
+                        try {
+                            os.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+            }
+        }.start();
+    }
+
+    /**
+     *
+     *
+     */
+    public void stopRecording() {
+        Log.d(TAG, "stop recording");
+        isRecording = false;
+    }
+
+    /**
+     * Initialize audio recorder
+     */
+    private void initAudioRecorder() throws IOException {
+        int bytesPerFrame = audioFormat.getBytesPerFrame();
 		/* Get number of samples. Calculate the buffer size (round up to the
 		   factor of given frame size) */
-		int frameSize = AudioRecord.getMinBufferSize(samplingRate,
-				channelConfig, audioFormat.getAudioFormat()) / bytesPerFrame;
-		if (frameSize % FRAME_COUNT != 0) {
-			frameSize = frameSize + (FRAME_COUNT - frameSize % FRAME_COUNT);
-			Log.d(TAG, "Frame size: " + frameSize);
-		}
-		
-		bufferSize = frameSize * bytesPerFrame;
+        int frameSize = AudioRecord.getMinBufferSize(samplingRate,
+                channelConfig, audioFormat.getAudioFormat()) / bytesPerFrame;
+        if (frameSize % FRAME_COUNT != 0) {
+            frameSize = frameSize + (FRAME_COUNT - frameSize % FRAME_COUNT);
+            Log.d(TAG, "Frame size: " + frameSize);
+        }
 
-		/* Setup audio recorder */
-		audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
-				samplingRate, channelConfig, audioFormat.getAudioFormat(),
-				bufferSize);
-		
-		// Setup RingBuffer. Currently is 10 times size of hardware buffer
-		// Initialize buffer to hold data
-		ringBuffer = new RingBuffer(10 * bufferSize);
-		buffer = new byte[bufferSize];
-		
-		// Initialize lame buffer
-		// mp3 sampling rate is the same as the recorded pcm sampling rate 
-		// The bit rate is 32kbps
-		SimpleLame.init(samplingRate, 1, samplingRate, BIT_RATE);
-		
-		// Initialize the place to put mp3 file
+        bufferSize = frameSize * bytesPerFrame;
+
+        /* Setup audio recorder */
+        audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                samplingRate, channelConfig, audioFormat.getAudioFormat(),
+                bufferSize);
+
+        // Setup RingBuffer. Currently is 10 times size of hardware buffer
+        // Initialize buffer to hold data
+        ringBuffer = new RingBuffer(10 * bufferSize);
+        buffer = new byte[bufferSize];
+
+        // Initialize lame buffer
+        // mp3 sampling rate is the same as the recorded pcm sampling rate
+        // The bit rate is 32kbps
+        SimpleLame.init(samplingRate, 1, samplingRate, BIT_RATE);
+
+        // Initialize the place to put mp3 file
 //		String externalPath = Environment.getExternalStorageDirectory()
 //				.getAbsolutePath();
-		
-		//开启追加
-		os = new FileOutputStream(mp3File,true);
 
-		// Create and run thread used to encode data
-		// The thread will 
-		encodeThread = new DataEncodeThread(ringBuffer, os, bufferSize);
-		encodeThread.start();
-		audioRecord.setRecordPositionUpdateListener(encodeThread, encodeThread.getHandler());
-		audioRecord.setPositionNotificationPeriod(FRAME_COUNT);
-	}
+        //开启追加
+        os = new FileOutputStream(mp3File, true);
 
-	public interface OnRecordListener{
-		void onStart();
-		void onStop();
-	}
+        // Create and run thread used to encode data
+        // The thread will
+        encodeThread = new DataEncodeThread(ringBuffer, os, bufferSize);
+        encodeThread.start();
+        audioRecord.setRecordPositionUpdateListener(encodeThread, encodeThread.getHandler());
+        audioRecord.setPositionNotificationPeriod(FRAME_COUNT);
+    }
 
-	public void setOnRecordListener(OnRecordListener listener){
-		this.mListener = listener;
-	}
+    public interface OnRecordListener {
+        void onStart();
+
+        void onStop();
+
+        void onRecording(int sampleRate,double volume);//采样率和音量（分贝）
+    }
+
+    public void setOnRecordListener(OnRecordListener listener) {
+        this.mListener = listener;
+    }
 }
